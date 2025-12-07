5c2b2e6097fe20a7e014828886f590f95ff84239
Vikrant Mulankar <vikrantmulankar389@gmail.com>
2025-12-07 18:28:40 +0530

    home page text change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,8 @@ const Home = () => {
 
         <div className="content-wrapper">
           <h1>
-            Charley Knox
+            {/* Charley Knox */}
+            Charley Vikrant
           </h1>
           <p>
             DRONE PHOTOGRAPHY
